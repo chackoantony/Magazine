aba0b0135883644aa53768f7173828c6210839a5
Sunil Antony <sunil.antony@loanstreet.com.my>
2017-02-19 02:19:25 +0800

    adding more tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
 
-  describe "POST #create" do
-    it "creates articles" do
+  describe 'POST #create' do
+    it 'creates articles' do
       sign_in
       post :create, {article: {title: 'sample', content: 'sample'} }
       expect(Article.count).to eq 1
@@ -15,6 +15,16 @@ RSpec.describe ArticlesController, type: :controller do
       post :create, {article: {title: 'sample', content: 'sample', tag_ids: tags.pluck(:id)} }
       expect(Article.last.tags.count).to eq 2
     end  
+  end
+
+  describe 'GET #get_sub_tags' do
+    it 'returns sub tags for a tag' do
+      tag = create(:tag)
+      sub_tag = create_list(:tag, 2, parent_id: tag.id) 
+      sign_in
+      get :get_sub_tags, {tag: tag.id }
+      expect(JSON.parse(response.body).count).to eq 2
+    end
   end
 
  
