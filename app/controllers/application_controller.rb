63c2ccd2c22ca0abd24253cd0969f74b5245e010
Sunil Antony <sunil.antony@loanstreet.com.my>
2017-02-19 04:22:12 +0800

    adding name to user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def record_not_found
     render 'shared/record_not_found'
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
   end
 end
